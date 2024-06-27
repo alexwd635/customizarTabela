ba43606c8795202a04eadfd07f2e9d091ebaf9a3
Alex Max <maxdesenv@gmail.com>
2024-06-27 16:31:30 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,59 @@
+var modal = document.getElementById('modal');
+var mascara = document.getElementById('mascara');
+
+var checkboxes = document.querySelectorAll("input[type='checkbox']");
+checkboxes.forEach(function(checkbox) {
+    checkbox.checked = true;
+});
+
+function fecharModal(){
+    modal.style.display = "none";
+    mascara.style.display = "none";
+}
+function abrirModal(){
+    modal.style.display = "block";
+    mascara.style.display = "block";
+}
+
+function selecaoDecolunas(val) {
+    var colunaForm = document.getElementsByName('coluna');
+    var colunas = document.querySelectorAll('td');
+
+    var listAlterar = [];
+
+    if(val == 1){
+        for (let index = 0; index < colunas.length; index++) {
+            colunas[index].classList.remove('hide');
+        }
+
+        for (let index = 0; index < colunaForm.length; index++) {
+            if(colunaForm[index].checked == false){
+                listAlterar.push(colunaForm[index].value);
+            }
+        }
+
+        listAlterar.forEach(hide=>{
+            colunas.forEach(item=>{
+                if(item.innerText == hide){
+                    item.classList.add('hide');
+                }
+            })
+        });
+
+    }else{
+        colunas.forEach(item=>{
+            item.classList.remove('hide');
+        })
+      
+        for (let index = 0; index < colunaForm.length; index++) {
+            if(colunaForm[index].checked == true){
+                colunaForm[index].checked == false;
+            }
+        }
+
+        var checkboxes = document.querySelectorAll("input[type='checkbox']");
+        checkboxes.forEach(function(checkbox) {
+            checkbox.checked = false;
+        });
+    }
+}
